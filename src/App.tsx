45ba44ef63e98911fb98996e1cd19b89069054f8
Stephen Krider <stephenjkrider@gmail.com>
2021-04-21 02:28:36 -0700

    fixes router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import {Box, Flex} from '@chakra-ui/react';
 import {
-	BrowserRouter as Router,
+	HashRouter as Router,
 	Switch,
 	Route
 } from "react-router-dom";
